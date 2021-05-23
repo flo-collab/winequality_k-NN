fafd2a1efbe24efba4db08789bc141d79bb98d6c
Florian Umrani <f.u.etudes@laposte.net>
2021-05-23 17:11:11 +0200

    cross-validation/select k/ accuracy

--- winequality_k-NN.py
+++ winequality_k-NN.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn import preprocessing
-
-
+from sklearn import neighbors, metrics
 
 data = pd.read_csv('winequality-white.csv', sep=";")
 
@@ -37,16 +36,16 @@ plt.show()
 '''
 # on transforme le probleme en pb de classification
 # les deux classes seront quality=6 et quality<6
-y_class = np.where(y<6, 0, 1)
+y_class = np.where(y < 6, 0, 1)
 
-X_train, X_test, y_treain, y_test = model_selection.train_test_split(X, y_class, test_size=0.3)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y_class, test_size=0.3)
 
 # Onstandardise les données d'entrainement et de test
 std_scale = preprocessing.StandardScaler().fit(X_train)
 X_train_std = std_scale.transform(X_train)
 X_test_std = std_scale.transform(X_test)
 
-
+'''
 # Affichage des données standardisées
 fig = plt.figure(figsize=(16, 12))
 for feat_idx in range(X_train_std.shape[1]):
@@ -54,6 +53,45 @@ for feat_idx in range(X_train_std.shape[1]):
     h = ax.hist(X_train_std[:, feat_idx], bins=50, color = 'steelblue', density=True, edgecolor='none')
     ax.set_title(data.columns[feat_idx], fontsize=14)
 plt.show()
+'''
+
+# Fixer les valeurs des hyperparamètres à tester
+param_grid = {'n_neighbors': [3, 5, 7, 9, 11, 13, 15, 17, 19]}
+
+# Choisir un score à optimiser, ici l'accuracy (proportion de prédictions correctes)
+score = 'accuracy'
+
+# Créer un classifieur kNN avec recherche d'hyperparamètre par validation croisée
+clf = model_selection.GridSearchCV(
+    neighbors.KNeighborsClassifier(),  # un classifieur kNN
+    param_grid,     # hyperparamètres à tester
+    cv=5,           # nombre de folds de validation croisée
+    scoring=score   # score à optimiser
+)
+
+# Optimiser ce classifieur sur le jeu d'entraînement
+clf.fit(X_train_std, y_train)
+
+# Afficher le(s) hyperparamètre(s) optimaux
+print("Meilleur(s) hyperparamètre(s) sur le jeu d'entraînement:")
+print(clf.best_params_)
+
+# Afficher les performances correspondantes
+print("Résultats de la validation croisée :")
+for mean, std, params in zip(
+    clf.cv_results_['mean_test_score'],  # score moyen
+    clf.cv_results_['std_test_score'],  # écart-type du score
+    clf.cv_results_['params']           # valeur de l'hyperparamètre
+):
+
+    print("{} = {:.3f} (+/-{:.03f}) for {}".format(
+        score,
+        mean,
+        std * 2,
+        params
+    ))
 
 
+y_pred = clf.predict(X_test_std)
+print("\nSur le jeu de test, l'accuracy est: {:.3f}".format(metrics.accuracy_score(y_test, y_pred)))
 
