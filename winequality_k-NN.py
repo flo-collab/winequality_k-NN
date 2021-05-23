bbd90a3db8ccf47511dc590e81e95f78f6f03ff5
Florian Umrani <f.u.etudes@laposte.net>
2021-05-23 14:26:40 +0200

    standardising datas - spliting best quality wine and others

--- winequality_k-NN.py
+++ winequality_k-NN.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn import preprocessing
+
+
+
 data = pd.read_csv('winequality-white.csv', sep=";")
 
 # Apercu du dataset:
+'''
 print(data.head())
 print(data.describe())
 print(data.shape)
 print(list(data.columns.values))
-
+'''
 # X contient les points(multidim) et y contient les etiquettes (dim 1,note de qualité)
 X = data[data.columns[:-1]].values
 y = data['quality']
@@ -29,3 +35,25 @@ for feat_idx in range(X.shape[1]):
     ax.set_title(data.columns[feat_idx], size=14)
 plt.show()
 '''
+# on transforme le probleme en pb de classification
+# les deux classes seront quality=6 et quality<6
+y_class = np.where(y<6, 0, 1)
+
+X_train, X_test, y_treain, y_test = model_selection.train_test_split(X, y_class, test_size=0.3)
+
+# Onstandardise les données d'entrainement et de test
+std_scale = preprocessing.StandardScaler().fit(X_train)
+X_train_std = std_scale.transform(X_train)
+X_test_std = std_scale.transform(X_test)
+
+
+# Affichage des données standardisées
+fig = plt.figure(figsize=(16, 12))
+for feat_idx in range(X_train_std.shape[1]):
+    ax = fig.add_subplot(3,4, (feat_idx+1))
+    h = ax.hist(X_train_std[:, feat_idx], bins=50, color = 'steelblue', density=True, edgecolor='none')
+    ax.set_title(data.columns[feat_idx], fontsize=14)
+plt.show()
+
+
+
